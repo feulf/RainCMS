b608989b53dcb7b939c626a4d9b0b78afa4817ea
Rain <rainelemental@gmail.com>
2012-06-29 19:37:29 -0400

    Added ajax and install files

---
+++ website/modules/content/content.ajax.php
@@ -0,0 +1,12 @@
+<?php
+
+    class ContentAjaxModule extends Module {
+
+        function read_content( $content_id ) {
+            $content = Content::get_content( $content_id );
+            echo json_encode( $content );
+        }
+
+    }
+
+    // -- end
