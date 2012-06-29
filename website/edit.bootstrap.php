2e802c359e310ee12524859bfc8d31d4ab316c1c
Rain <rainelemental@gmail.com>
2012-06-28 22:17:48 -0400

    Added edit mode

---
+++ website/edit.bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+    require LIBRARY_DIR . "RainEditPageLoader.php";
+
+    // init loader
+    $loader = new RainEditPageLoader();
+
+    $loader->load_plugins( "edit_mode" );
+    
+    $loader->init_session();
+
+    $loader->init_db();
+
+    $loader->init_settings();
+
+    $loader->init_language();
+
+    $loader->auth_user();
+
+    $loader->init_content();
+
+    $loader->init_theme();
+
+    $loader->load_head();
+
+    $loader->load_module();
+
+    $loader->load_blocks();
+
+    $loader->load_menu();
+
+    $loader->draw();
+
+
+
+    // -- end
