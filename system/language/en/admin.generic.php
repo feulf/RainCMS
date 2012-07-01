ff8d39abfa0734289f2f019b3562d54ccd5b4993
Rain <rainelemental@gmail.com>
2012-07-01 11:29:29 -0400

    Added admin.generic language

--- system/language/en/admin.generic.php
+++ system/language/en/admin.generic.php
@@ -1,12 +1,57 @@
 <?php
 
     global $lang;
-    $lang["page not found"] = "Page not found";
-    $lang["content not found"] = "Content not found";
-    $lang["module not found"] = "Module not found";
-    $lang["theme not found"] = "Theme not found";
-    $lang["search no results"] = "Search gave no results";
-    $lang["read more"] = "Read more";
+
+    // admin menu
+    $lang["admin_menu_website"] = "Website";
+    $lang["admin_menu_conf"] = "Configuration";
+    $lang["admin_menu_content"] = "Contents";
+    $lang["admin_menu_product"] = "Orders";
+    $lang["admin_menu_mailer"] = "Mailer";
+    $lang["admin_menu_user"] = "Users";
+    $lang["admin_menu_exit"] = "Logout";
+
+
+    // content type
+    $lang["type_content"]   = "Content";
+    $lang["type_sitemap"] = "Site Map";
+    $lang["type_login"] = "Login Page";
+    $lang["type_registration"] = "Registration Page";
+    $lang["type_search"] = "Search Page";
+    $lang["type_contact"] = "Contact Us Page";
+    $lang["type_news_section"] = "News Section";
+    $lang["type_news"] = "News";
+    $lang["type_article_section"] = "Article Section";
+    $lang["type_article"] = "Articles";
+    $lang["type_blog_section"] = "Blog Section";
+    $lang["type_blog"] = "Blog";
+    $lang["type_event_section"] = "Event Section";
+    $lang["type_event"] = "Event";
+    $lang["type_gallery_section"] = "Gallery Section";
+    $lang["type_gallery_category"] = "Category";
+    $lang["type_archive_section"] = "File Archive";
+    $lang["type_archive_category"] = "Category";
+    $lang["type_archive_file"] = "File";
+    $lang["type_product_section"] = "Product Section";
+    $lang["type_product_category"] = "Category";
+    $lang["type_product"] = "Product";
+    $lang["type_cloud"] = "Tag Cloud";
+    $lang["type_forum"] = "Forum";
+    $lang["type_forum_category"] = "Forum Category";
+    $lang["type_forum_discussion"] = "Forum Discussion";
+    $lang["type_documentation"] = "Documentation";
+
+    // generic
+    $lang["edit_page"] = "Edit Page";
+    $lang["control_panel"] = "Control Panel";
+    $lang["drag_drop"] = "Order with drag drop.";
+    $lang["content_type_uknown"] = "Content type uknown";
+    $lang["file_type_uknown"] = "File type uknown";
+    $lang["file_type_not_allowed"] = "File type not allowed. Extensions allowed:";
+    $lang["file_exceed_size"] = "Maximum file size:";
+    $lang["file_msg_del"] = "Are you sure you want delete this file?";
+    $lang["file_msg_editname"] = "Insert the file name.";
+    $lang["file_size"] = "File size";
 
 
 
