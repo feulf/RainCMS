60590b2b94311a60c077810b1ec91ebfa06efe79
Rain <rainelemental@gmail.com>
2012-07-01 11:29:10 -0400

    Change of menu position

---
+++ system/language/en/admin.generic.php
@@ -0,0 +1,13 @@
+<?php
+
+    global $lang;
+    $lang["page not found"] = "Page not found";
+    $lang["content not found"] = "Content not found";
+    $lang["module not found"] = "Module not found";
+    $lang["theme not found"] = "Theme not found";
+    $lang["search no results"] = "Search gave no results";
+    $lang["read more"] = "Read more";
+
+
+
+    // -- end
