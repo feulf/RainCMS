029c618bad24bf528041d535d0fae62dd5a112b5
Rain <rainelemental@gmail.com>
2012-07-08 13:49:51 -0400

    DB change

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@
     $db['dev']['username'] = 'root';
     $db['dev']['password'] = 'root';
     $db['dev']['database'] = 'rain';
+    $db['dev']['db_prefix'] = '';
 
     // production database (live website)
     $db['www.raincms.com']['driver'] = '';
@@ -13,13 +14,17 @@
     $db['www.raincms.com']['username'] = '';
     $db['www.raincms.com']['password'] = '';
     $db['www.raincms.com']['database'] = '';
+    $db['www.raincms.com']['db_prefix'] = '';
 
-    if (!defined("DB_PREFIX"))
-        define("DB_PREFIX", "");
     if (isset($_SERVER['SERVER_NAME']) && array_key_exists($_SERVER['SERVER_NAME'], $db)) {
         define("DEFAULT_CONNECTION_NAME", $_SERVER['SERVER_NAME']);
     } else {
         define("DEFAULT_CONNECTION_NAME", "dev");
     }
+
+    // Database table prefix
+    define( "DB_PREFIX", $db[ DEFAULT_CONNECTION_NAME ]["db_prefix" ] );
+
+
 
     // -- end
