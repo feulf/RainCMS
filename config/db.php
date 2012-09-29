cd67d9e4991f4830f682063a429d1393fad0ccca
Rain <rainelemental@gmail.com>
2012-09-29 15:35:14 -0400

    configuration file fix

--- config/db.php
+++ config/db.php
@@ -1,30 +1,30 @@
-<?php
+<?
 
+$db = array(
     // development database (default)
-    $db['dev']['driver'] = 'mysql';
-    $db['dev']['hostname'] = 'localhost';
-    $db['dev']['username'] = 'root';
-    $db['dev']['password'] = 'root';
-    $db['dev']['database'] = 'rain';
-    $db['dev']['db_prefix'] = '';
+    'dev' => array(
+        'driver' => 'mysql',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'root',
+        'database' => 'rain'
+    ),
+    //production database (live website)
+    'raincms.com' => array(
+        'driver' => '',
+        'hostname' => '',
+        'username' => '',
+        'password' => '',
+        'database' => ''
+    )
+);
 
-    // production database (live website)
-    $db['www.raincms.com']['driver'] = '';
-    $db['www.raincms.com']['hostname'] = '';
-    $db['www.raincms.com']['username'] = '';
-    $db['www.raincms.com']['password'] = '';
-    $db['www.raincms.com']['database'] = '';
-    $db['www.raincms.com']['db_prefix'] = '';
-
-    if (isset($_SERVER['SERVER_NAME']) && array_key_exists($_SERVER['SERVER_NAME'], $db)) {
-        define("DEFAULT_CONNECTION_NAME", $_SERVER['SERVER_NAME']);
-    } else {
-        define("DEFAULT_CONNECTION_NAME", "dev");
-    }
-
-    // Database table prefix
-    define( "DB_PREFIX", $db[ DEFAULT_CONNECTION_NAME ]["db_prefix" ] );
+if (isset($_SERVER['SERVER_NAME']) && array_key_exists($_SERVER['SERVER_NAME'], $db)) 
+    define("DEFAULT_CONNECTION_NAME", $_SERVER['SERVER_NAME']);
+else
+    define("DEFAULT_CONNECTION_NAME", "dev");
 
 
-
-    // -- end
+ if (!defined("DB_PREFIX"))
+    define ("DB_PREFIX", "");
+// -- end
