f2bd0c42b42a1f6a8b79794d3d8ec11197ab5672
Federico <federico@poisonfx.com>
2012-12-01 17:47:51 -0500

    update module search

--- website/modules/search/search.php
+++ website/modules/search/search.php
@@ -2,21 +2,23 @@
 
     class SearchModule extends Module {
 
-        function draw() {
-
-            $search = $this->get("search");
+        function index( $search ) {
 
             if ($search) {
 
-                $content_list = DB::get_all("SELECT c.*, f.filepath AS cover, f.thumb AS cover_thumbnail, MATCH( title, subtitle, content, tags ) AGAINST ( :search ) AS score, CONCAT( '" . URL . "',path) AS link
-                                             FROM " . DB_PREFIX . "content AS c
-                                             JOIN " . DB_PREFIX . "content_type AS t ON c.type_id = t.type_id
-                                             LEFT JOIN ".DB_PREFIX."file f ON f.rel_id=c.content_id AND f.status=".FILE_COVER."
-                                             WHERE MATCH ( title, subtitle, content, tags ) AGAINST ( :search )
-                                             AND c.published = 1 AND t.searchable = 1 AND lang_id=:lang_id
-                                             GROUP BY content_id
-                                             ORDER BY c.type_id, score DESC", array(":search" => $search, ":lang_id" => LANG_ID));
+                $content_list = DB::get_all("SELECT c.*, CONCAT( '" . URL . "',path) AS link,
+                                                    f.filepath AS cover, f.thumb AS cover_thumbnail,
+                                                    MATCH( c.title, c.subtitle, c.content, c.tags ) AGAINST ( :search ) AS score
 
+                                             FROM " . DB_PREFIX . "content c
+                                             JOIN " . DB_PREFIX . "content_type t ON c.type_id = t.type_id
+                                             LEFT JOIN ".DB_PREFIX."file_rel fr ON fr.rel_id=c.content_id AND fr.rel_type=".FILE_COVER."
+                                             LEFT JOIN ".DB_PREFIX."file f ON f.file_id=fr.file_id
+                                             WHERE MATCH ( c.title, c.subtitle, c.content, c.tags ) AGAINST ( :search )
+                                             AND c.published = 1 AND t.searchable=1 AND lang_id=:lang_id
+                                             GROUP BY content_id
+                                             ORDER BY c.type_id, score DESC", 
+                                             array(":search" => $search, ":lang_id" => LANG_ID));
 
 
                 // if no results
@@ -51,11 +53,16 @@
             else
                 $content_list = 1;
 
-            $view = new View();
-            $view->assign('search', $search);
-            $view->assign($this->get_content());
-            $view->assign("content_list", $content_list);
-            $view->draw($this->get_template());
+            $this->assign('search', $search);
+            $this->assign($this->get_content());
+            $this->assign("content_list", $content_list);
+            $this->set_layout("layout.search");
+        }
+        
+        function filter( $action, $params ){
+            $search = $action;
+            $this->index( $search );
+            
         }
 
     }
