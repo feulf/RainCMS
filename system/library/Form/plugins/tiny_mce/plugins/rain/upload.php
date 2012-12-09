42ae28551c7e3b36ad878dcf41cc17158562420a
Federico <federico@poisonfx.com>
2012-12-09 16:36:46 -0500

    but fix, upload image in content

--- system/library/Form/plugins/tiny_mce/plugins/rain/upload.php
+++ system/library/Form/plugins/tiny_mce/plugins/rain/upload.php
@@ -18,59 +18,44 @@
 			$v = str_replace( "'", "\\'", $v );
 			$param_encoded .= $param_encoded ? ",$k:'$v'" : "$k:'$v'";
 		}
-		
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>upload</title>
-		<!-- style -->
-		
-		<!-- included javascript -->
-		<script src="<?php echo ROOT_DIR . JQUERY_DIR; ?>/jquery.min.js" type="text/javascript"></script>
+	
+?><!DOCTYPE html>
+<html>
+    <head>
+	<title>upload</title>
+        
+        <script src="<?php echo ROOT_DIR . JQUERY_DIR; ?>/jquery.min.js" type="text/javascript"></script>
         <script src="<?php echo ROOT_DIR . ADMIN_JAVASCRIPT_DIR; ?>ajaxupload.js" type="text/javascript"></script>
+        <script src="<?php echo ROOT_DIR . ADMIN_JAVASCRIPT_DIR; ?>file.js" type="text/javascript"></script>
         <script src="<?php echo ROOT_DIR . ADMIN_JAVASCRIPT_DIR; ?>content.js" type="text/javascript"></script>
         <script src="<?php echo ROOT_DIR . LIBRARY_DIR; ?>Form/plugins/tiny_mce/tiny_mce_popup.js" type="text/javascript"></script>
 
-  		<!-- javascript -->
-		<script type="text/javascript">
+  	<!-- javascript -->
+	<script type="text/javascript">
 
-			$(document).ready( function(){
-					new AjaxUpload('#upload_cover_button', {
-			  			action: '<?php echo ROOT_DIR; ?>admin.ajax.php/Content/upload_image_content',
-			  			name: 'file',
-			  			autoSubmit: false,
-						responseType: 'json',
-						onChange: function( file, ext ){
-							
-							<?php
-								echo "this.setData({ $param_encoded });";
-							?>
-							if( ext != 'jpg' && ext != 'jpeg' && ext != 'png' && ext != 'gif' )
-								return alert( 'File not allowed' );
-							this.submit()
-						}
-						,
-						onComplete: function( file, response ){
-							if( response['result'] )
-								file_insert_content( response['file_id'], response['dir'], response['filepath'] )
-							else
-								alert( response['msg'] )
-						}
-					});
-			});
+            $(document).ready( function(){
+                new AjaxUpload('#upload_cover_button', {
+                                action: '<?php echo ROOT_DIR; ?>admin.ajax.php/Content/upload_image_content',
+                                name: 'file',
+                                autoSubmit: false,
+                                responseType: 'json',
+                                onChange: function( file, ext ){
+                                                                <?php echo "this.setData({ $param_encoded });"; ?>
+                                                                if( ext != 'jpg' && ext != 'jpeg' && ext != 'png' && ext != 'gif' )
+        								return alert( 'File not allowed' );
+                    						this.submit()
+                                                               },
+				onComplete: function( file, response ){
+                                                    if( response['result'] )
+                                                        file_insert_content( response['file_id'], response['dir'], response['filepath'] )
+                                                    else
+                                                        alert( response['msg'] )
+				}
+		});
+            });
 		</script>
 </head>
-	<body style="margin:10px;">
-            <!--
-		<div id="upload_cover_button" class="upload_cover_button"><img src="img/upload.gif" alt="upload"/></div>
-            -->
-                
-            <div  class="upload_cover_button">
-                <button id="upload_cover_button" class="cover-upload">Upload Cover</button>
-            </div>
-            <a class="cover-choose" href="javascript:parent.cover_choose(<?php echo get('content_id'); ?>)">Choose a cover</a>
-	</body>
+    <body style="margin:10px;">
+        <div id="upload_cover_button" class="upload_cover_button"><img src="img/upload.gif" alt="upload"/></div>    
+    </body>
 </html>
